8d082aaf33e4107d367f28ea134b73a834638902
huanglongpu <306315964@qq.com>
2019-04-27 17:55:27 +0800

    优化命名

--- src/main/java/com/obatis/net/factory/HttpHandleFactory.java
+++ src/main/java/com/obatis/net/factory/HttpHandleFactory.java
@@ -3,6 +3,7 @@ package com.obatis.net.factory;
 import com.obatis.constant.NormalCommonConstant;
 import com.obatis.constant.http.DefaultHttpConstant;
 import com.obatis.net.HttpResponseResult;
+import com.obatis.validate.ValidateTool;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
@@ -37,19 +38,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class HttpConnectionFactory {
+public class HttpHandleFactory {
 
 	/**
 	 * 默认的HTPTP 超时时间
 	 */
-	private static final int DEFAULT_CONNECT_TIME_OUT = 150000;
-	protected static int SETTING_CONNECT_TIME_OUT = DEFAULT_CONNECT_TIME_OUT;
-	protected static int TIME_OUT = SETTING_CONNECT_TIME_OUT;
+	private static final int DEFAULT_TIME_OUT = 150000;
+	protected static int SETTING_TIME_OUT = DEFAULT_TIME_OUT;
+	protected static int TIME_OUT = SETTING_TIME_OUT;
 	
-	private HttpConnectionFactory() {
+	private HttpHandleFactory() {
 	}
 
-	private static PoolingHttpClientConnectionManager connectionManager = null;
+	private static PoolingHttpClientConnectionManager manager = null;
 
 	static {
 		SSLContext context = null;
@@ -62,18 +63,18 @@ public class HttpConnectionFactory {
 		final HostnameVerifier verifier = new DefaultHostnameVerifier();
 		final Registry<ConnectionSocketFactory> register = RegistryBuilder.<ConnectionSocketFactory> create()
 				.register("http", PlainConnectionSocketFactory.INSTANCE).register("https", new SSLConnectionSocketFactory(context, verifier)).build();
-		connectionManager = new PoolingHttpClientConnectionManager(register);
+		manager = new PoolingHttpClientConnectionManager(register);
 		// 设置连接池的最大连接数
-		connectionManager.setMaxTotal(200);
+		manager.setMaxTotal(150);
 		// 一个路由的最大连接数，尽量保证性能
-		connectionManager.setDefaultMaxPerRoute(connectionManager.getMaxTotal());
+		manager.setDefaultMaxPerRoute(manager.getMaxTotal());
 	}
 
 	private static CloseableHttpClient buildHttpClient(CookieStore cookieStore) {
 
 		HttpClientBuilder builder = HttpClients.custom();
 		builder.setConnectionManagerShared(true);
-		builder.setConnectionManager(connectionManager);
+		builder.setConnectionManager(manager);
 		if(cookieStore != null) {
 			builder.setDefaultCookieStore(cookieStore);
 		}
@@ -81,7 +82,7 @@ public class HttpConnectionFactory {
 	}
 
 
-	protected static HttpResponseResult load(String url, Map<String, Object> param, CookieStore cookieStore, String method, Map<String, Object> headers) {
+	protected static HttpResponseResult load(String url, Map<String, Object> params, CookieStore cookie, String method, Map<String, Object> headers) {
 
 		if (url.endsWith("/")) {
 			url = url.substring(0, url.length() - 1);
@@ -90,12 +91,12 @@ public class HttpConnectionFactory {
 			url = url.substring(0, url.length() - 1);
 		}
 
-		HttpUriRequest request = getRequestMethod(param, url, method, headers);
-		return load(request, cookieStore);
+		HttpUriRequest request = setRequestParam(url, params, method, headers);
+		return load(request, cookie);
 	}
 
 	// 以下代码为连接
-	private static HttpResponseResult load(HttpUriRequest request, CookieStore cookieStore) {
+	private static HttpResponseResult load(HttpUriRequest request, CookieStore cookie) {
 
 		CloseableHttpClient client = null;
 		String content = null;
@@ -104,12 +105,12 @@ public class HttpConnectionFactory {
 		HttpEntity entity = null;
 		try {
 			
-			if(cookieStore == null) {
-				cookieStore = new BasicCookieStore();
+			if(cookie == null) {
+				cookie = new BasicCookieStore();
 			}
-			client = buildHttpClient(cookieStore);
+			client = buildHttpClient(cookie);
 			HttpClientContext context = new HttpClientContext();
-			context.setCookieStore(cookieStore);
+			context.setCookieStore(cookie);
 			
 			// 执行请求
 			response = client.execute(request, context);
@@ -121,7 +122,7 @@ public class HttpConnectionFactory {
 			e.printStackTrace();
 		} finally {
 			if (entity != null) {
-				// 将entity彻底完全消费掉
+				// 关闭数据流
 				EntityUtils.consumeQuietly(entity);
 			}
 			if (response != null) {
@@ -155,12 +156,14 @@ public class HttpConnectionFactory {
 		HttpResponseResult result = new HttpResponseResult();
 		result.setStatus(status);
 		result.setResult(content);
-		result.setCookie(cookieStore);
+		result.setCookie(cookie);
 		return result;
 	}
 
-	private static RequestConfig getRequestConfig() {
-		// 加载请求配置
+	private static RequestConfig setRequestTimeOutConfig() {
+		/**
+		 * 设置 HTTP 请求超时
+		 */
 		Builder builder = RequestConfig.custom();
 		builder.setConnectTimeout(TIME_OUT);
 		builder.setConnectionRequestTimeout(TIME_OUT);
@@ -168,47 +171,46 @@ public class HttpConnectionFactory {
 		return builder.build();
 	}
 
-	private static HttpUriRequest getRequestMethod(Map<String, Object> param, String url, String method, Map<String, Object> headers) {
+	private static HttpUriRequest setRequestParam(String url, Map<String, Object> params, String method, Map<String, Object> headers) {
 
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		List<NameValuePair> param = new ArrayList<>();
 
-		if (param != null) {
-			Set<Map.Entry<String, Object>> entrySet = param.entrySet();
+		if (params != null) {
+			Set<Map.Entry<String, Object>> entrySet = params.entrySet();
 			for (Map.Entry<String, Object> e : entrySet) {
 				NameValuePair pair = new BasicNameValuePair(e.getKey(), (e.getValue() != null) ? e.getValue().toString() : "");
-				params.add(pair);
+				param.add(pair);
 			}
 		}
 
-		RequestBuilder requestBuilder = null;
+		RequestBuilder builder = null;
 		if (DefaultHttpConstant.REQ_METHOD_POST.equals(method)) {
 			// post请求
-			requestBuilder = RequestBuilder.post(url);
+			builder = RequestBuilder.post(url);
 		} else {
 			// get请求
-			requestBuilder = RequestBuilder.get(url);
+			builder = RequestBuilder.get(url);
 		}
 		
 		// 设置header
 		if(headers != null) {
 			for (Map.Entry<String, Object> entry : headers.entrySet()) {
-				Object value = entry.getValue();
-				if(value == null) {
+				if(ValidateTool.isEmpty(entry.getValue())) {
 					continue;
 				}
-				requestBuilder.addHeader(entry.getKey(), value.toString());
+				builder.addHeader(entry.getKey(), entry.getValue().toString());
 			}
 		}
 		
 		// 设置超时时间
-		requestBuilder.setConfig(getRequestConfig());
+		builder.setConfig(setRequestTimeOutConfig());
 		// 设置编码
-		requestBuilder.setCharset(Charset.forName(NormalCommonConstant.CHARSET_UTF8));
+		builder.setCharset(Charset.forName(NormalCommonConstant.CHARSET_UTF8));
 		
 		// 设置参数
-		if (params.size() > 0) {
-			requestBuilder.addParameters(params.toArray(new BasicNameValuePair[params.size()]));
+		if (param.size() > 0) {
+			builder.addParameters(param.toArray(new BasicNameValuePair[param.size()]));
 		}
-		return requestBuilder.build();
+		return builder.build();
 	}
 }
--- src/main/java/com/obatis/net/factory/HttpHandler.java
+++ src/main/java/com/obatis/net/factory/HttpHandler.java
@@ -17,7 +17,7 @@ public class HttpHandler {
 	 */
 	public static void setConnectionTimeOut(int timeout) throws RuntimeException {
 		if(timeout > 0) {
-			HttpConnectionFactory.SETTING_CONNECT_TIME_OUT = timeout;
+			HttpHandleFactory.SETTING_TIME_OUT = timeout;
 		} else {
 			throw new RuntimeException("error:timeout value invalid");
 		}
@@ -35,71 +35,71 @@ public class HttpHandler {
 	/**
 	 * HTTP POST 方式请求。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @return
 	 */
-	public static HttpResponseResult post(String url, Map<String, Object> param) {
-		return post(url, param, null, null);
+	public static HttpResponseResult post(String url, Map<String, Object> params) {
+		return post(url, params, null, null);
 	}
 	
 	/**
 	 * HTTP POST 方式请求，支持传入 Cookie 信息。
 	 * 该方法于 1.2.1 弃用，改由 postWithCookie 方法替代。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore cookie信息
+	 * @param params 请求参数
+	 * @param cookie cookie信息
 	 * @return
 	 */
 	@Deprecated
-	public static HttpResponseResult post(String url, Map<String, Object> param, CookieStore cookieStore) {
-		return post(url, param, cookieStore, null);
+	public static HttpResponseResult post(String url, Map<String, Object> params, CookieStore cookie) {
+		return post(url, params, cookie, null);
 	}
 	
 	/**
 	 * HTTP POST 方式请求，支持传入 Cookie 信息。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore cookie信息
+	 * @param params 请求参数
+	 * @param cookie cookie信息
 	 * @return
 	 */
-	public static HttpResponseResult postCookie(String url, Map<String, Object> param, CookieStore cookieStore) {
-		return post(url, param, cookieStore, null);
+	public static HttpResponseResult postCookie(String url, Map<String, Object> params, CookieStore cookie) {
+		return post(url, params, cookie, null);
 	}
 	
 	/**
 	 * HTTP POST 方式请求，支持传入 Header 信息。
 	 * 该方法于 1.2.1 弃用，改由 postWithHeader 方法替代。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @param headers header信息
 	 * @return
 	 */
 	@Deprecated
-	public static HttpResponseResult post(String url, Map<String, Object> param, Map<String, Object> headers) {
-		return post(url, param, null, headers);
+	public static HttpResponseResult post(String url, Map<String, Object> params, Map<String, Object> headers) {
+		return post(url, params, null, headers);
 	}
 	
 	/**
 	 * HTTP POST 方式请求，支持传入 Header 信息。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @param headers header信息
 	 * @return
 	 */
-	public static HttpResponseResult postHeader(String url, Map<String, Object> param, Map<String, Object> headers) {
-		return post(url, param, null, headers);
+	public static HttpResponseResult postHeader(String url, Map<String, Object> params, Map<String, Object> headers) {
+		return post(url, params, null, headers);
 	}
 	
 	/**
 	 * HTTP POST 方式请求
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore cookie信息
+	 * @param params 请求参数
+	 * @param cookie cookie信息
 	 * @param headers header信息
 	 * @return
 	 */
-	public static HttpResponseResult post(String url, Map<String, Object> param, CookieStore cookieStore, Map<String, Object> headers) {
-		return HttpConnectionFactory.load(url, param, cookieStore, DefaultHttpConstant.REQ_METHOD_POST, headers);
+	public static HttpResponseResult post(String url, Map<String, Object> params, CookieStore cookie, Map<String, Object> headers) {
+		return HttpHandleFactory.load(url, params, cookie, DefaultHttpConstant.REQ_METHOD_POST, headers);
 	}
 	
 	/**
@@ -114,70 +114,70 @@ public class HttpHandler {
 	/**
 	 * HTTP GET 方式请求。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @return
 	 */
-	public static HttpResponseResult get(String url, Map<String, Object> param) {
-		return get(url, param, null, null);
+	public static HttpResponseResult get(String url, Map<String, Object> params) {
+		return get(url, params, null, null);
 	}
 	
 	/**
 	 * HTTP GET 方式请求，支持传入cookie。
 	 * 该方法于 1.2.1 弃用，改由 getWithCookie 方法替代。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore  cookie信息
+	 * @param params 请求参数
+	 * @param cookie  cookie信息
 	 * @return
 	 */
 	@Deprecated
-	public static HttpResponseResult get(String url, Map<String, Object> param, CookieStore cookieStore) {
-		return get(url, param, cookieStore, null);
+	public static HttpResponseResult get(String url, Map<String, Object> params, CookieStore cookie) {
+		return get(url, params, cookie, null);
 	}
 	
 	/**
 	 * HTTP GET 方式请求，支持传入cookie。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore  cookie信息
+	 * @param params 请求参数
+	 * @param cookie  cookie信息
 	 * @return
 	 */
-	public static HttpResponseResult getCookie(String url, Map<String, Object> param, CookieStore cookieStore) {
-		return get(url, param, cookieStore, null);
+	public static HttpResponseResult getCookie(String url, Map<String, Object> params, CookieStore cookie) {
+		return get(url, params, cookie, null);
 	}
 	
 	/**
 	 * HTTP GET 方式请求，支持传入Header。
 	 * 该方法于 1.2.1 弃用，改由 getWithHeader 方法替代。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @param headers header信息
 	 * @return
 	 */
 	@Deprecated
-	public static HttpResponseResult get(String url, Map<String, Object> param, Map<String, Object> headers) {
-		return get(url, param, null, headers);
+	public static HttpResponseResult get(String url, Map<String, Object> params, Map<String, Object> headers) {
+		return get(url, params, null, headers);
 	}
 	
 	/**
 	 * HTTP GET 方式请求，支持传入Header。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
+	 * @param params 请求参数
 	 * @param headers header信息
 	 * @return
 	 */
-	public static HttpResponseResult getHeader(String url, Map<String, Object> param, Map<String, Object> headers) {
-		return get(url, param, null, headers);
+	public static HttpResponseResult getHeader(String url, Map<String, Object> params, Map<String, Object> headers) {
+		return get(url, params, null, headers);
 	}
 	
 	/**
 	 * HTTP GET 方式请求。
 	 * @param url HTTP 请求url地址
-	 * @param param 请求参数
-	 * @param cookieStore  cookie信息
+	 * @param params 请求参数
+	 * @param cookie  cookie信息
 	 * @param headers header信息
 	 * @return
 	 */
-	public static HttpResponseResult get(String url, Map<String, Object> param, CookieStore cookieStore, Map<String, Object> headers) {
-		return HttpConnectionFactory.load(url, param, cookieStore, DefaultHttpConstant.REQ_METHOD_GET, headers);
+	public static HttpResponseResult get(String url, Map<String, Object> params, CookieStore cookie, Map<String, Object> headers) {
+		return HttpHandleFactory.load(url, params, cookie, DefaultHttpConstant.REQ_METHOD_GET, headers);
 	}
 }
